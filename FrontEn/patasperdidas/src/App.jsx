05e8711b45c9d0b794658eaf86af34838da78bb4
DanteBell95 <dante.belli18@gmail.com>
2025-06-20 12:25:36 -0300

    Se agrega el llamado al navbar en app,jsx

--- FrontEn/patasperdidas/src/App.jsx
+++ FrontEn/patasperdidas/src/App.jsx
@@ -4,11 +4,12 @@ import Mapa from "./pages/Mapa";
 import NuevaMascota from "./pages/NuevaMascota";
 import CerrarCaso from "./pages/CerrarCaso";
 import { AuthProvider } from "./context/AuthContext";
-
+import NavBar from "./components/NavBar";
 export default function App(){
   return(
     <AuthProvider>
       <BrowserRouter>
+        <NavBar/>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/mapa" element={<Mapa />} />
